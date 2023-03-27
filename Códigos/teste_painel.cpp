00e244b202d91f5bde4e256dcc38118fb9c37fa8
Gabriel Lemes Molizane Almeida <85410063+GabrielLMA@users.noreply.github.com>
2023-03-27 15:01:44 -0300

    Update teste_painel.cpp

--- Códigos/teste_painel.cpp
+++ Códigos/teste_painel.cpp
@@ -1,8 +1,10 @@
-
+#include <Arduino.h>
+#include <Servo.h>
 //Teste de leitura da placa
-int sol = 0; 
+int sol = 0; //Intensidade luminosa
 
 void setup() {
+  pinMode(A2, INPUT);  //Painel solar conectado em A2
   Serial.begin(9600);           
 }
 
