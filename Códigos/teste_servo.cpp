32b7e3ff16f84b1b396b98a0c2731e30bcbafccc
Gabriel Lemes Molizane Almeida <85410063+GabrielLMA@users.noreply.github.com>
2023-03-27 15:06:16 -0300

    Update teste_servo.cpp

--- Códigos/teste_servo.cpp
+++ Códigos/teste_servo.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 #include <Servo.h>
+//Teste do servomotor
+int servo = 0;  //Valor de ângulo do servo em graus
 
-int servo = 0;
-
-Servo servo_10;
+Servo servo_10; //Definição do servomotor
 
 void setup()
 {
-  servo_10.attach(10, 500, 2500);
+  servo_10.attach(10, 500, 2500); //Servomotor conectado ao pino D10
   Serial.begin(9600);
 }
 
@@ -15,6 +15,6 @@ void loop()
 {
   servo = servo_10.read();
   Serial.println(servo);
-  servo_10.write(90);
+  servo_10.write(90); //Coloca o servomotor na posição de 90°
   delay(1000);
 }
